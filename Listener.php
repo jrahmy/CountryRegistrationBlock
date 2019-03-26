25f28857e0ab39d70d0591c7898f1a3c8a1d2a7b
Jeremy P <jrahmy+git@gmail.com>
2019-03-26 16:34:01 -0700

    Code clean-up

--- Listener.php
+++ Listener.php
@@ -3,8 +3,6 @@
 /*
  * This file is part of a XenForo add-on.
  *
- * (c) Jeremy P <https://xenforo.com/community/members/jeremy-p.450/>
- *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -13,56 +11,46 @@ namespace Jrahmy\CountryRegistrationBlock;
 
 /**
  * Contains event listener callbacks.
- *
- * @author Jeremy P <https://xenforo.com/community/members/jeremy-p.450/>
  */
 class Listener
 {
     /**
-     * A flag indicating whether or not the visitor's country is blocked from
-     * registering.
-     *
      * @var bool
      */
     protected static $countryBlocked = false;
 
     /**
-     * Disables registration for guest users visiting from a blocked country.
-     *
      * @param \XF\Pub\App $app
      */
     public static function appPubSetup(\XF\Pub\App $app)
     {
         $visitor = \XF::visitor();
+        if ($visitor->user_id !== 0) {
+            return;
+        }
 
-        if ($visitor->user_id === 0) {
-            $options = $app->options();
+        $options = $app->options();
 
-            $blockOptions = $options->jCRB;
-            $geoIpHeader = $blockOptions['geoIpHeader'];
-            $disallowedCountries = $blockOptions['disallowedCountries'];
+        $blockOptions = $options->jCRB;
+        $geoIpHeader = $blockOptions['geoIpHeader'];
+        $disallowedCountries = preg_split(
+            '/\r?\n/',
+            $blockOptions['disallowedCountries'],
+            PREG_SPLIT_NO_EMPTY
+        );
+        if (!empty($_SERVER[$geoIpHeader])) {
+            static::$countryBlocked = in_array(
+                $_SERVER[$geoIpHeader],
+                $disallowedCountries
+            );
+        }
 
-            if (!empty($_SERVER[$geoIpHeader])) {
-                $country = $_SERVER[$geoIpHeader];
-                $blockedCountries = preg_split(
-                    '/\r?\n/',
-                    $disallowedCountries,
-                    PREG_SPLIT_NO_EMPTY
-                );
-
-                static::$countryBlocked = in_array($country, $blockedCountries);
-            }
-
-            if (static::$countryBlocked) {
-                $options->registrationSetup['enabled'] = 0;
-            }
+        if (static::$countryBlocked) {
+            $options->registrationSetup['enabled'] = 0;
         }
     }
 
     /**
-     * Appends a notice to the notice list if a guest user is visiting from a
-     * blocked country.
-     *
      * @param \XF\Pub\App    $app
      * @param \XF\NoticeList $noticeList
      * @param array          $pageParams
@@ -73,14 +61,15 @@ class Listener
         array $pageParams
     ) {
         $visitor = \XF::visitor();
+        if ($visitor->user_id !== 0) {
+            return;
+        }
 
-        if (($visitor->user_id === 0) and static::$countryBlocked) {
-            $templater = $app->templater();
-
+        if (static::$countryBlocked) {
             $noticeList->addNotice(
                 'j_crb_blocked',
                 'block',
-                $templater->renderTemplate(
+                $app->templater()->renderTemplate(
                     'public:j_crb_notice_blocked',
                     $pageParams
                 )
